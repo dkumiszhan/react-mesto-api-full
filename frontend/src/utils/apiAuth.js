36142f9ee40ce4334ed79ec208602198942488fa
Kumiszhan Dybyspayeva <d.kumiszhan@gmail.com>
2022-10-02 02:53:43 +0100

    One more fix to baseUrl

--- frontend/src/utils/apiAuth.js
+++ frontend/src/utils/apiAuth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "http://kumiszhan.students.nomoredomains.icu/api";
 
 const checkResponse = (response) => {
   return response.ok
